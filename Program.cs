9d2e62e9bf2f31c687c7032154c35cbf20377417
CamaelVentura <camael.ventura@gmail.com>
2021-01-25 19:18:24 -0300

    Projeto zerado

--- Program.cs
+++ Program.cs
@@ -6,13 +6,7 @@ namespace DigitalInovationOne
     {
         static void Main(string[] args)
         {
-            var s = new Pilha();
-            s.Empilha(1);
-            s.Empilha(10);
-            s.Empilha(100);
-            Console.WriteLine(s.Desempilha());
-            Console.WriteLine(s.Desempilha());
-            Console.WriteLine(s.Desempilha());
+            
         }
     }
 }
